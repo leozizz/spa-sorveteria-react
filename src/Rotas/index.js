c7783949168ba4eda0fa29ccd754f8a86c04ef42
leozizz <leonardocps2015@gmail.com>
2023-02-16 16:09:06 -0300

    Estrutura inicial da pagina

---
+++ src/Rotas/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from "../componentes/paginas/Home"
+import Sabores from "../componentes/paginas/Sabores"
+import Sobre from "../componentes/paginas/Sobre"
+
+
+const Rotas = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path exact='/' element={<Home/>}/>
+            <Route path='/sabores' element={<Sabores/>}/>
+            <Route path='/sobre' element={<Sobre/>}/>
+        </Routes>
+    </BrowserRouter>
+);
+
+export default Rotas;
---
+++ src/componentes/paginas/Sabores/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Sabores = () => (
+    <div>
+        <p>Sabores</p>
+    </div>
+)
+
+export default Sabores;
