813d65165eabfd5f1687a52d0b243cbc03f43b05
leozizz <leonardocps2015@gmail.com>
2023-02-23 16:15:04 -0300

    Aplicando responsividade

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -4,25 +4,25 @@ import './style.css'
 const Rodape = () => {
     return (
         <footer>
-            <div>
-                <img src="assets/logo.png"/>
-                <div>
+            <div className="footer-container">
+                <img className="logo" src="assets/logo.png"/>
+                <div className="item-container">
                     <h4>ENDEREÇO</h4>
                     <p>Av. Bernadino de Campos, 98</p>
                     <p>São Paulo, SP 12345-678</p>
                 </div>
-                <div>
+                <div className="item-container">
                     <h4>CONTATO</h4>
                     <p>info@meusite.com</p>
                     <p>Tel: (11) 3456-7890</p>
                 </div>
-                <div>
+                <div className="item-container">
                     <h4>HORÁRIOS</h4>
                     <p>ABERTO TODOS OS DIAS</p>
                     <p>10:00 - 22:00</p>
                 </div>
             </div>
-            <div>
+            <div className="copy">
                 <p>Gelateria. Orgulhosamente desenvolvido por "Leozizz"</p>
             </div>
         </footer>
